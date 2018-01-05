159ae8e49c6b20890ecb963133b42848c9c62bb1
1nfrag <sil3ntcll@gmail.com>
2018-01-05 20:23:41 +0800

    Full import old camera
    Change-Id: Id5a3b962f8a9ff177802656526c86252fc5a4902

--- drivers/media/platform/msm/camera_v2/fd/msm_fd_dev.c
+++ drivers/media/platform/msm/camera_v2/fd/msm_fd_dev.c
@@ -339,10 +339,9 @@ static int msm_fd_vbif_error_handler(void *handle, uint32_t error)
 	struct msm_fd_buffer *active_buf;
 	int ret;
 
-	if (NULL == handle) {
-		dev_err(fd->dev, "FD Ctx is null, Cannot recover\n");
+	if (handle == NULL)
 		return 0;
-	}
+
 	ctx = (struct fd_ctx *)handle;
 	fd = (struct msm_fd_device *)ctx->fd_device;
 
@@ -1055,14 +1054,18 @@ static int msm_fd_s_ctrl(struct file *file, void *fh, struct v4l2_control *a)
 			a->value = ctx->format.size->work_size;
 		break;
 	case V4L2_CID_FD_WORK_MEMORY_FD:
+		mutex_lock(&ctx->fd_device->recovery_lock);
 		if (ctx->work_buf.fd != -1)
 			msm_fd_hw_unmap_buffer(&ctx->work_buf);
 		if (a->value >= 0) {
 			ret = msm_fd_hw_map_buffer(&ctx->mem_pool,
 				a->value, &ctx->work_buf);
-			if (ret < 0)
+			if (ret < 0) {
+				mutex_unlock(&ctx->fd_device->recovery_lock);
 				return ret;
+			}
 		}
+		mutex_unlock(&ctx->fd_device->recovery_lock);
 		break;
 	default:
 		return -EINVAL;
